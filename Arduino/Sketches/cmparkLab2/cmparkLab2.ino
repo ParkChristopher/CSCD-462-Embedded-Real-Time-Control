bb47624854b078cadb1f523b1fddeeeb45fe3a84
Chris Park <christopherpark@eagles.ewu.edu>
2016-10-05 00:07:11 -0700

    Lab 2 completed
    ...

--- Arduino/Sketches/cmparkLab2/cmparkLab2.ino
+++ Arduino/Sketches/cmparkLab2/cmparkLab2.ino
@@ -3,41 +3,52 @@
 
 #define READ_PIN 0
 #define WRITE_PIN 9
+#define DELTA 100
+#define NUM_SAMPLES 40
+#define NUM_INDICES 256
 
 unsigned int _minLedValue, _maxLedValue;
 unsigned long _settlingTime;
+bool _isVerbose;
+int _lookupTable[NUM_INDICES];
 
 /*-----------------------------------------------------*/
 
 void setup() {
   Serial.begin(9600);
-  
-  _settlingTime = getSettlingTimeMillis();
-  calculateExtremes();
-  
+
+  _isVerbose = true;
 }//end setup
 
 /*-----------------------------------------------------*/
 
 void loop() {
 
-  Serial.println();
-  Serial.println("Enter input to begin linear mapping.");
-  Serial.println();
-  while(Serial.available()==0){}
+  requestInput("Calculate Settling Time");
+  _settlingTime = getSettlingTimeMillis();
+  clearBuffer();
   
+  requestInput("Calculate Extreme Values");
+  calculateExtremes();
+  clearBuffer();
+  
+  requestInput("Linear Mapping and Calibration Loop");
+  initLookupTable();
   linearMappingLoop();
-  
-  Serial.read();
+  clearBuffer();
+
+  requestInput("Lookup Mapping Test Loop");
+  lookupTestLoop();
+  clearBuffer();
 }//end loop
 
 /*-----------------------------------------------------*/
 
 int getSampleVoltage(){
   int i;
-  unsigned int res = 0;
+  unsigned long res = 0;
   
-  for(i = 0; i < 20; i++){
+  for(i = 0; i < NUM_SAMPLES; i++){
     res += analogRead(READ_PIN);
   }
   return res / (i + 1);
@@ -53,12 +64,13 @@ unsigned long getSettlingTimeMillis(){
   isSettled = false;
   prevSample = currSample = 0;
   analogWrite(WRITE_PIN, 0);
-  delay(3000);
+  delay(1000);
   startTime = millis();
   analogWrite(WRITE_PIN, 127);
 
   while(!isSettled){
     currSample = getSampleVoltage();
+    Serial.println(currSample);
       
     if(currSample > prevSample)
       prevSample = currSample;
@@ -68,8 +80,8 @@ unsigned long getSettlingTimeMillis(){
   
   endTime = millis();
   sTime = endTime - startTime;
-  sTime += 50;
-  Serial.print("Settling Time: ");
+  sTime += DELTA;
+  Serial.print("Settling Time (msec): ");
   Serial.println(sTime);
   return sTime;
 }//end estimateSettlingtime
@@ -81,18 +93,15 @@ void calculateExtremes(){
   analogWrite(WRITE_PIN, 0);
   delay(_settlingTime);
   _minLedValue = getSampleVoltage();
-  Serial.print("Minimum reading: ");
+  Serial.print("Minimum Value: ");
   Serial.println(_minLedValue);
 
   //Get maximum brightness reading
   analogWrite(WRITE_PIN, 255);
   delay(_settlingTime);
   _maxLedValue = getSampleVoltage();
-  Serial.print("Maximum reading: ");
+  Serial.print("Maximum Value: ");
   Serial.println(_maxLedValue);
-
-  //Reset the LED
-  analogWrite(WRITE_PIN, 0);
 }//end calculateExtremes
 
 /*-----------------------------------------------------*/
@@ -100,30 +109,103 @@ void calculateExtremes(){
 void linearMappingLoop(){
   int i, sample;
 
-  for(i = 0; i < 256; i++){
+  for(i = 0; i < NUM_INDICES; i++){
     analogWrite(WRITE_PIN, i);
     delay(_settlingTime);
 
-    Serial.print("LED=");
+    if(_isVerbose)
+      Serial.print("LED=");
     Serial.print(i);
-    Serial.print(" ");
+    Serial.print(", ");
     
     sample = getSampleVoltage();
-    sample = constrain(sample, _minLedValue, _maxLedValue);
 
-    Serial.print("Light=");
+    if(_isVerbose)
+      Serial.print("Light=");
     Serial.print(sample);
-    Serial.print(" ");
+    Serial.print(", ");
     
-    sample = map(sample, 0, 1023, 0, 255); //Is this the correct use?
+    sample = map(sample, _minLedValue, _maxLedValue, 0, 255);
+    sample = constrain(sample, 0, 255);
     
-    Serial.print("LinMap=");
+    if(_isVerbose)
+      Serial.print("LinearMapping=");
     Serial.println(sample);
+
+    _lookupTable[sample] = i;
     
-  }
+  }//end for loop
   
-  //Reset the LED
-  analogWrite(WRITE_PIN, 0);
+  backFillLookupTable();
 }//end linearGuessLoop
+
+/*-----------------------------------------------------*/
+
+void lookupTestLoop(){
+  int i, sample;
+
+  for(i = 0; i < NUM_INDICES; i++){
+    analogWrite(WRITE_PIN, i);
+    delay(_settlingTime);
+
+    if(_isVerbose)
+      Serial.print("LED=");
+    Serial.print(i);
+    Serial.print(", ");
+    
+    sample = getSampleVoltage();
+   
+    if(_isVerbose)
+      Serial.print("Light=");
+    Serial.print(sample);
+    Serial.print(", ");
+
+    sample = map(sample, _minLedValue, _maxLedValue, 0, 255);
+    sample = constrain(sample, 0, 255);
+    
+    if(_isVerbose)
+      Serial.print("ReverseMapping=");
+    Serial.println(_lookupTable[sample]);
+  }//end for loop
+  
+}//end lookupTestLoop
+
+/*-----------------------------------------------------*/
+
+void initLookupTable(){
+  for(int i = 0; i < NUM_INDICES; i++)
+    _lookupTable[i] = -1;
+}//end initLookupTable
+
+/*-----------------------------------------------------*/
+
+void backFillLookupTable(){
+
+  //if the first index is -1, set it to zero
+  if(_lookupTable[0] == -1)
+    _lookupTable[0] = 0;
+    
+  for(int i = 1; i < NUM_INDICES; i++)
+    if(_lookupTable[i] == -1)
+      _lookupTable[i] = _lookupTable[i - 1];
+}//end backFillLookupTable
+
+/*-----------------------------------------------------*/
+
+void clearBuffer(){
+  while(Serial.available() > 0)
+    Serial.read();
+  analogWrite(WRITE_PIN, 0);
+  Serial.println();
+}//end clearBuffer
+
+/*-----------------------------------------------------*/
+
+void requestInput(char* taskStr){
+  Serial.print("Next task: ");
+  Serial.println(taskStr);
+  Serial.println("Enter input to continue...");
+  while(Serial.available() <= 0);
+}//end requestInput
 
 /*-----------------------------------------------------*/
