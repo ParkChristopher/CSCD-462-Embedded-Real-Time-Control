d907eee5dd4519fdb3f2b731177d905bc84c98f7
ParkChristopher <christopherpark@eagles.ewu.edu>
2016-10-27 00:32:49 -0700

    Adde pinouts, began display logic
    All of the output pins for the display are defined and set. Began
    fleshing out the logic for updating the display digits.

--- Arduino/Sketches/cmparkminiproject1/cmparkminiproject1.ino
+++ Arduino/Sketches/cmparkminiproject1/cmparkminiproject1.ino
@@ -1,58 +1,101 @@
 //Chris Park
 //Mini Project 1
+
 #define NUM_DISPLAYS 5
 #define NUM_DIGITS 7
-
-unsigned volatile int _scoreArray[NUM_DISPLAYS][NUM_DIGITS];
-unsigned int _currentDigit;
-/*
-Task:
-Create a test program on the pinball machine that
--cycles the score "simultaneously" on all 5 score displays
-from 0 through 999999
-updating once ever 500ms
-with a circular increment of 8353, and an offset between displays of 8353
-*/
-
-
 //NOTE: When you set up the Interrupt the, use rising edge
 //ie. attachInterrupt(digitalPinToInterrupt(2), ISRFunction, RISING);
+
+
+//Program flow:
+//Digits should update as fast as interrupt 2 can fire,
+//a timer should be used to update score data every 500ms.
+/*----------------------------------------------------*/
+//Output Pins
+
+#define STROBE_DISPLAY5 38
+#define ENABLE_STROBE 88    //A9
+#define BLANK_DISPLAY 82    //A15
+#define ENABLE_DIGIT_1 39   //Digit enables ordered LSB to MSB
+#define ENABLE_DIGIT_2 40
+#define ENABLE_DIGIT_3 41
+#define ENABLE_DIGIT_4 50
+#define ENABLE_DIGIT_5 51
+#define ENABLE_DIGIT_6 52
+#define ENABLE_DIGIT_7 53
+
+/*----------------------------------------------------*/
+
+unsigned volatile int _scoreArray[NUM_DISPLAYS][NUM_DIGITS];
+
+/*----------------------------------------------------*/
+
 void setup() {
-  //Set PORTA pins to output
-  pinMode(29, OUTPUT);
-  pinMode(28, OUTPUT);
-  pinMode(27, OUTPUT);
-  pinMode(26, OUTPUT);
-  pinMode(25, OUTPUT);
-  pinMode(24, OUTPUT);
-  pinMode(23, OUTPUT);
-  pinMode(22, OUTPUT);
-  pinMode(38, OUTPUT);
-
-  //Set display 5 strobe to output
-  pinMode(38, OUTPUT);
-  //Set display strobe enable to output
-  pinMode(A9, OUTPUT);
-  //Set display blanking pin to output
-  pinMode(A15, OUTPUT);
-
-  //Set 7 Digit enable pins to output (Order is MSB to LSB/ Top Down)
-  pinMode(39, OUTPUT);
-  pinMode(40, OUTPUT);
-  pinMode(41, OUTPUT);
-  pinMode(50, OUTPUT);
-  pinMode(51, OUTPUT);
-  pinMode(52, OUTPUT);
-  pinMode(53, OUTPUT);
+  configureOutputPins();
 }
 
-//write an interrupt service routine
+/*----------------------------------------------------*/
+
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
 
+/*----------------------------------------------------*/
 
-void setScore(){}
-void parseScore(){}
+void configureOutputPins(){
+  DDRA = B11111111;                   //PORTA
+  pinMode(STROBE_DISPLAY5,  OUTPUT);
+  pinMode(BLANK_DISPLAY,    OUTPUT);
+  pinMode(ENABLE_STROBE,    OUTPUT);
+  pinMode(ENABLE_DIGIT_1,   OUTPUT);
+  pinMode(ENABLE_DIGIT_2,   OUTPUT);
+  pinMode(ENABLE_DIGIT_3,   OUTPUT);
+  pinMode(ENABLE_DIGIT_4,   OUTPUT);
+  pinMode(ENABLE_DIGIT_5,   OUTPUT);
+  pinMode(ENABLE_DIGIT_6,   OUTPUT);
+  pinMode(ENABLE_DIGIT_7,   OUTPUT);
+}
+
+/*----------------------------------------------------*/
+void setScore(unsigned int display, unsigned int score){
+  //_scoreArray[i] = parseScore(score);
+}
+
+/*----------------------------------------------------*/
+
+void updateDisplays(){
+  static unsigned int currentDigit;
+  
+  digitalWrite(BLANK_DISPLAY, HIGH);    //Blank all displays
+  digitalWrite(currentDigit, LOW);      //Disable previous digit
+  //Update current digit variable
+  //Enable display strobes by setting A9 high
+  //for each of the five displays
+    //set lower nibble of porta to BCD value to display.
+    //toggle the displays strobe line hight then low
+  //disable the display strobe by setting a9 low
+  //set the enable bit for the new digit high (use current digit here?)
+  //set the display blanking bit low (a15) this turns on all displays
+}
+
+/*----------------------------------------------------*/
+
+//Note: if the score is greater than seven digits, we should
+//not return it. it will exceed the maximum digits of the display.
+int* parseScore(unsigned int score){
+  //break the score down into individual digits
+  //adds them to an integer array
+  //returns that array.
+} 
+
+/*----------------------------------------------------*/
+
+unsigned int uIntToBCD(unsigned int val){
+  int bcdVal[4] = {0,0,0,0};
+  return bcdVal;  
+}
+
+/*----------------------------------------------------*/
+
 void setDisplay(){}
+
+/*----------------------------------------------------*/
