99a6a7974af30f5f5d2c522e8b33f54a4a02ee5c
Chris Park <christopherpark@eagles.ewu.edu>
2016-10-29 03:59:36 -0700

    ..

--- Arduino/Sketches/cmparkminiproject1/tests.ino
+++ Arduino/Sketches/cmparkminiproject1/tests.ino
@@ -15,17 +15,3 @@
   setScore(displayNum, mCurrentScores[displayNum]);
   displayNum = displayNum < NUM_DISPLAYS ? displayNum++ : 0;
  }
-
-void updateScoreInterrupt() {
-
-  bool done = false;
-  for (uint8_t i = 0; i < NUM_DISPLAYS && !done; i++) {
-
-    mCurrentScores[i] ++;
-    setScore(i, mCurrentScores[i]);
-
-    if(mCurrentScores[i] > 8353) {
-      done = true;
-    }
-  }
-}
